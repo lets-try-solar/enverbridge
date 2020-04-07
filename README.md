d828f1f362e307f46a043bbc101f065412d82983
hemmi <lts@lets-try-solar.de>
2020-04-07 09:59:03 +0200

    readme updated and get_solar.pl added

--- README.md
+++ README.md
@@ -1,2 +1,80 @@
+# Enverbridge stats collector
 
-# enverbridge
+| Lets-try-Solar | [Website][Lts] |
+
+get_solar.pl is a script to collect stats from the Envertec portal.
+
+# Setup 
+
+#### Clone the repository to your machine
+```
+git clone https://github.com/lets-try-solar/enverbridge.git
+```
+#### Enter the directory
+```sh
+cd enverbridge
+```
+#### Create a configuration file
+
+  - id: keep it empty (the script will collect the stationID from the envertec portal automatically)
+  - dbcon: IP of your InfluxDB host
+  - database: InfluxDB database name
+  - influxtag: Influx tag for the metrics
+  - username: The email address you are using to login to the Envertec portal
+  - password: The password which you use on the Envertec portal
+
+#### Configuration file example
+
+```sh
+vi envertech_config.json
+```
+```
+{
+    "id" : "",
+    "dbcon" : "INFLUXDB:8086",
+    "database" : "enverbridge",
+    "influxtag" : "enverbridge",
+    "username" : "EMAIL",
+    "password" : "PASSWORD"
+}
+```
+
+#### Make the script executable
+
+ ```sh
+ chmod +x get_solar.pl
+```
+
+# Execute the script
+
+```sh
+./get_solar.pl envertech_config.json
+```
+
+### Sample output
+```
+StationID: 1234
+Capacity: 0.0
+Etoday: 0.00
+InvTotal: 0
+power: 000.00
+PowerStr: 000.00
+income: 000.00
+StrPeakPower: 000.00
+monthpower: 00.00
+daypower: 0.00
+allpower: 0.00
+yearpower: 000.00
+```
+
+### Todos
+
+ - Bundle script into Docker container
+
+License
+----
+
+MIT
+
+   [Lts]: <https://www.lets-try-solar.de>
+
---
+++ get_solar.pl
@@ -0,0 +1,121 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use diagnostics;
+use LWP::Simple;                	# From CPAN
+use JSON qw( decode_json );     	# From CPAN
+use Data::Dumper;               	# Perl core module
+use JSON::Parse 'parse_json';
+use Encode qw(decode encode);
+use JSON;
+use File::Basename;
+
+my $dirname = dirname(__FILE__);
+
+# declare the perl command line flags/options we want to allow
+my $configfile=$ARGV[0];
+
+my $configjson;
+{
+  local $/; #Enable 'slurp' mode
+  open my $fh, "<", $configfile;
+  $configjson = <$fh>;
+  close $fh;
+}
+
+# config json example enverbridge_config.json
+#{
+#	"id" : ,
+#	"dbcon" : "",
+#	"database" : "",
+#	"influxtag" : "",
+#	"username" : "",
+#	"password" : ""
+#}
+
+my $configdata = decode_json($configjson);
+
+my $id=$configdata->{'id'};				# ID from the Solarsystem
+my $dbcon=$configdata->{'dbcon'};			# InfluxDB connection details
+my $database=$configdata->{'database'};			# InfluxDB Database name
+my $influxtag=$configdata->{'influxtag'};		# InfluxDB tag
+my $username=quotemeta($configdata->{'username'});	# Envertech portal username
+my $password=quotemeta($configdata->{'password'});	# Envertech portal password
+
+# mapping as the variable names have changed
+my %mapping = (
+	Power => 'power',
+	StrIncome => 'income',
+	UnitEMonth => 'monthpower',
+	UnitEToday => 'daypower',
+	UnitETotal => 'allpower',
+	UnitEYear => 'yearpower',
+);
+
+# check if StationID is available from config file, if not query it from Envertec portal and update config file
+if ($id eq "")
+{
+	get_stationID();
+	print "\n";
+	print "\{ \"id\" : \"$id\", \"dbcon\" : \"$dbcon\", \"database\" : \"$database\", \"influxtag\" : \"$influxtag\", \"username\" : \"$configdata->{'username'}\", \"password\" : \"$configdata->{'password'}\" \}";
+	open my $fh, ">", $configfile;
+	print $fh "\{ \"id\" : \"$id\", \"dbcon\" : \"$dbcon\", \"database\" : \"$database\", \"influxtag\" : \"$influxtag\", \"username\" : \"$configdata->{'username'}\", \"password\" : \"$configdata->{'password'}\" \}";
+	close $fh;
+	print "\n\n";
+}
+
+print "StationID: $id\n";
+
+# login to get cookie
+my $cookie = qx(curl --silent --cookie-jar /tmp/cookies -o /dev/null -X POST -H "Content-Type: application/json" -X "Content-Length: 1000" 'https://www.envertecportal.com/apiaccount/login?username=$username&pwd=$password');
+# get data from portal
+my $response = qx(curl --silent --cookie /tmp/cookies -X POST -d "" 'https://www.envertecportal.com/ApiStations/getStationInfo?stationId=$id');
+
+my $ref_hash = decode_json($response);
+
+my $value;
+foreach my $item($ref_hash->{'Data'}){
+	delete $item->{'CreateYear'};
+	delete $item->{'CreateMonth'};
+	delete $item->{'Lng'}; 
+	delete $item->{'TimeZone'};
+	delete $item->{'UnitCapacity'};
+	delete $item->{'Installer'};
+	delete $item->{'CreateTime'};
+	delete $item->{'PwImg'};
+	delete $item->{'Lat'};
+	delete $item->{'StationName'};
+	delete $item->{'StrTrees'};
+	delete $item->{'StrCO2'};
+	foreach my $key (sort keys %{$item}) { 
+		$item->{$key} =~ s/[^\d\.]//g; 
+		$value = encode('UTF-8',$item->{$key});
+		if ( exists $mapping{$key} ) {
+			$key = $mapping{$key};
+		}
+		print "$key: $value\n";
+		system "curl --output /dev/null --silent -i -XPOST 'http://$dbcon/write?db=$database' --data-binary '$key,tag=$influxtag value=$value'";
+	}
+}
+
+# Get StationID from Envertec portal
+sub get_stationID {
+	system "wget -qO- -o /dev/null --keep-session-cookies --save-cookies $dirname/cookies.txt --post-data 'username=$configdata->{'username'}&pwd=$configdata->{'password'}' https://www.envertecportal.com/apiaccount/login > /dev/null 2>&1";
+	system "wget -q --load-cookies $dirname/cookies.txt 'https://www.envertecportal.com/terminal/systemoverview' -O $dirname/systemoverview > /dev/null 2>&1";
+
+	my $sofile = "$dirname/systemoverview";
+	open my $fh, '<', $sofile or die "Could not open '$sofile' $!\n";
+	my $idtemp;
+	while (my $line = <$fh>) {
+        	chomp $line;
+        	my @strings = $line =~ /(?:^|\s)(var stationId =+.*$)/g;
+        	foreach my $s (@strings) {
+                	$idtemp = $s;
+        	}
+	}
+	close($fh);
+	system "rm $dirname/systemoverview";
+	my @parts = split '\'', $idtemp;
+	$id = $parts[1];
+}
